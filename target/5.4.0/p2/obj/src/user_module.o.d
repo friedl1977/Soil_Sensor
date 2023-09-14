0f9e6de336a44f47530059cbef304566b64dcd82
Friedl Basson <friedlb@icloud.com>
2023-09-14 15:49:28 +0200

    older DeviceOS versions depreciated

--- target/5.4.0/p2/obj/src/user_module.o.d
+++
@@ -1,6 +0,0 @@
-/Users/friedl/Desktop/Projects/Soil_Sensor/target/5.4.0/p2//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/rtl872x/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-../../../modules/shared/rtl872x/inc/user-part/user_module.inc:
-../../../dynalib/inc/dynalib.h:
